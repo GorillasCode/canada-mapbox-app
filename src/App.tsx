798b29a5562440fbfdb40e1c68afc48ba33b7e1e
Haenell <haenellfilho@hotmail.com>
2025-04-24 15:08:37 -0300

    MAPBOXCAD-0029: Add Forgot Password and Reset Password pages with routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/auth/LoginPage";
 import MainApp from "./components/MainApp";
 import Register from "./components/auth/Register";
+import ForgotPasswordPage from "./components/auth/ForgotPasswordPage";
+import ResetPasswordPage from "./components/auth/ResetPasswordPage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/app" element={<MainApp />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
       </Routes>
     </Router>
   );
